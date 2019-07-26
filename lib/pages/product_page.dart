9c42fe13833a5ed81e7ac213fbe21ce7efabb5f9
joaoh <joao_camargo10@hotmail.com>
2019-07-25 22:01:09 -0300

    Product page layout

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -16,34 +16,66 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          title: const Text("New product"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.remove,
-              ),
-              onPressed: () {},
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/images/login_background.jpg"),
+            fit: BoxFit.cover,
+          )),
+          child: Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: (){
+                            Navigator.pop(context);
+                          },
+                        ),
+                        SizedBox(width: 10.0,),
+                        Text("New product", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.remove),
+                          onPressed: (){
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.save),
+                          onPressed: (){
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      padding: EdgeInsets.all(16),
+                      children: <Widget>[],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            IconButton(
-              icon: Icon(
-                Icons.save,
-              ),
-              onPressed: () {},
-            )
-          ],
-        ),
-        body: Form(
-          key: _formKey,
-          child: ListView(
-            padding: EdgeInsets.all(16),
-            children: <Widget>[],
           ),
         ),
-
+      ),
     );
   }
 }
